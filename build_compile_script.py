1aa263f3a809bea1852e6f850a5b8fbbe464e30c
Thomas L Randall <tlranda@clemson.edu>
2023-11-21 16:00:58 -0600

    Add reassembly/optimization options

--- build_compile_script.py
+++ build_compile_script.py
@@ -11,6 +11,8 @@ def build():
     prs.add_argument('--include', default="/home/trandall/ytune_2022/ytopt_tlranda/ytopt/benchmark/syr2k_exp", help="Include directories to utilize in compilation")
     prs.add_argument('--IR', action='store_true', help="Only build IR's")
     prs.add_argument('--AS', action='store_true', help="Only do IR assembly")
+    prs.add_argument('--DIS', action='store_true', help="Only do Bitcode Disassembly")
+    prs.add_argument('--with-opt', action='store_true', help="Add optimization to Bitcode Disassembly")
     return prs
 
 def parse(prs=None, args=None):
@@ -18,8 +20,17 @@ def parse(prs=None, args=None):
         prs = build()
     if args is None:
         args = prs.parse_args()
+    # Don't do more than one of these at once
+    exclusives = ["IR", "AS", "DIS"]
+    if sum([getattr(args, e) for e in exclusives]) > 1:
+        multipleExclusiveOptions = f"Can only specify one of {', '.join(['--'+e for e in exclusives])} at a time"
+        raise ValueError(multipleExclusiveOptions)
+    # Keep relative path
     if args.AS and args.clang[-7:] != "llvm-as":
         args.clang = str(pathlib.Path(args.clang).parent.joinpath("llvm-as"))
+    elif args.DIS and args.clang[-8:] != "llvm-dis":
+        args.clang = str(pathlib.Path(args.clang).parent.joinpath("llvm-dis"))
+    # Convert these attributes to pathlib Paths
     for attr in ['output_script', 'input_dir', 'clang', 'include']:
         setattr(args, attr, pathlib.Path(getattr(args, attr)))
     return args
@@ -31,7 +42,7 @@ def main(args=None):
         cmd_template = "{} {} polybench.c -I{} -DPOLYBENCH_TIME -std=c99 -fno-unroll-loops -O3 "+\
                        "-mllvm -polly -mllvm -polly-process-unprofitable "+\
                        "-mllvm -polly-use-llvm-names -ffast-math -march=native -S -emit-llvm"
-    elif args.AS:
+    elif args.AS or args.DIS:
         cmd_template = "{} {} -o {}"
     else:
         cmd_template = "{} {} polybench.c -I{} -DPOLYBENCH_TIME -std=c99 -fno-unroll-loops -O3 "+\
@@ -49,6 +60,12 @@ def main(args=None):
                 cmd = cmd_template.format(args.clang,
                                           fname.with_suffix('.ll'),
                                           fname.with_suffix('.bc'))
+            elif args.DIS:
+                cmd = cmd_template.format(args.clang,
+                                          fname.with_suffix('.bc'),
+                                          fname.with_name(fname.stem+'_reassembled.ll'))
+                if args.with_opt:
+                    cmd += f"; {args.clang.with_name('opt')} -S -O3 {fname.with_name(fname.stem+'_reassembled.ll')} -o {fname.with_name(fname.stem+'_optimized.ll')}"
             else:
                 cmd = cmd_template.format(args.clang,
                                           fname,
