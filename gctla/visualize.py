4c2237b6674b6510cb8c20ddd11e326dbcc23099
Thomas L Randall <tlranda@clemson.edu>
2024-01-25 13:41:37 -0500

    Add inference files and update visualizaiton script

--- gctla/visualize.py
+++ gctla/visualize.py
@@ -24,17 +24,25 @@ def build():
                         help="Files to use for plotting")
     iocntrl.add_argument("--ignore", nargs="*", default=None,
                         help="Files to not use for plotting that may be globbed in inputs list (default %(default)s)")
+    iocntrl.add_argument("--relabel", nargs="*", default=None,
+                        help="Provide new plot labels for input files (default: filename without path)")
     iocntrl.add_argument("--combine-by-directory", action="store_true",
                         help="Conglomerate files from the same directory into same DataFrame (default %(default)s)")
     iocntrl.add_argument("--oracle-rank", default=None,
                         help="Use this CSV as an oracle to rank input configurations rather than their objective value")
+    iocntrl.add_argument("--export", default=None,
+                        help="Instead of displaying plot, save to path (default display only, do not save)")
     iocntrl.add_argument("--format", choices=["png", "pdf", "svg", "jpeg"], default="png",
                         help="Export format for figures (default %(default)s)")
     pcntrl = prs.add_argument_group("Plot Controls")
+    pcntrl.add_argument("--title", default=None,
+                        help="Provide a figure title (default: %(default)s)")
     pcntrl.add_argument("--legend-loc", choices=legend_codes, default='best',
                         help="Where to place legend in plot (default %(default)s)")
     pcntrl.add_argument("--fig-pts", type=float, default=None,
                         help="Create figure size in LaTeX points using golden ratio (default %(default)s)")
+    pcntrl.add_argument("--indicate-budget", type=int, default=None,
+                help="")
     return prs
 
 def parse(args=None, prs=None):
@@ -52,6 +60,11 @@ def parse(args=None, prs=None):
             accepted_inputs.append(fname)
     args.inputs = accepted_inputs
     del args.ignore
+    # Relabeling
+    if args.relabel is None:
+        args.relabel = [None] * len(args.inputs)
+    if len(args.relabel) != len(args.inputs):
+        raise ValueError(f"Given {len(args.inputs)} inputs '{args.inputs}' and {len(args.relabel)} labels '{args.relabel}'. Must have one label per input!")
     # Set figure size
     args.fig_size = None
     if args.fig_pts is not None:
@@ -59,8 +72,6 @@ def parse(args=None, prs=None):
     del args.fig_pts
     if args.oracle_rank is not None:
         args.oracle_rank = pd.read_csv(args.oracle_rank).sort_values(by=['objective']).reset_index(drop=True)
-    # Show effective arguments
-    print(args)
     return args
 
 def set_size(width, fraction=1, subplots=(1,1)):
@@ -83,7 +94,8 @@ def oracle_rank(fname, args):
             full_match_idx = np.where(n_column_matches == len(columns))[0]
             assert len(full_match_idx) == 1
             rerank.append(full_match_idx[0])
-        data.loc[:,'objective'] = rerank
+        data['objective'] = rerank
+        #data.loc[:,'objective'] = rerank
     return data
 
 def load(args):
@@ -112,8 +124,11 @@ def load(args):
             new_dict[top_level_key] = combine.sort_values(by=['objective']).reset_index(drop=True)
         load_dict = new_dict
     # Debug: Show CSV counts for each loaded item
-    for key, val in load_dict.items():
-        print(f"Loaded {len(val)} items under alias '{key}'")
+    for ((key, val), relabel) in zip(load_dict.items(), args.relabel):
+        if relabel is None:
+            print(f"Loaded {len(val)} items under alias '{key}'")
+        else:
+            print(f"Loaded {len(val)} items from '{key}' under alias '{relabel}'")
     return load_dict
 
 def prepare_fig(args):
@@ -125,20 +140,32 @@ def prepare_fig(args):
         ax.set_ylabel("Oracle Rank")
     else:
         ax.set_ylabel("Objective Value")
+    if args.title is not None:
+        ax.set_title(args.title)
     return fig, ax
 
-def plot_source(fig, ax, idx, source, label, args):
+def plot_source(fig, ax, idx, source, label, relabel, args):
     color = mcolors[idx % len(mcolors)]
-    ax.plot(source['objective'], label=label, marker='o', color=color)
+    if relabel is None:
+        plot_label = pathlib.Path(label).name
+    else:
+        plot_label = relabel
+    ax.plot(source['objective'], label=plot_label, marker='o', color=color)
 
 def main(args=None):
     args = parse(args)
     loaded_dict = load(args)
     fig, ax = prepare_fig(args)
-    for idx, (label, data) in enumerate(loaded_dict.items()):
-        plot_source(fig, ax, idx, data, label, args)
+    for idx, ((label, data), relabel) in enumerate(zip(loaded_dict.items(), args.relabel)):
+        plot_source(fig, ax, idx, data, label, relabel, args)
     ax.legend(loc=args.legend_loc)
-    plt.show()
+    if args.export is None:
+        plt.show()
+    else:
+        if pathlib.Path(args.export).suffix != args.format:
+            args.export = pathlib.Path(args.export).with_suffix('.'+args.format)
+        print(f"Save figure to {args.export} as {args.format}")
+        fig.savefig(args.export, format=args.format, dpi=700)
 
 if __name__ == '__main__':
     main()
