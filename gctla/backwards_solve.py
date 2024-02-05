21feb4c3e73a8617c632f6ee6cc8482edb73d3d8
Thomas L Randall <tlranda@clemson.edu>
2024-02-05 17:17:14 -0500

    Feature updates to backwards solve

--- gctla/backwards_solve.py
+++ gctla/backwards_solve.py
@@ -2,7 +2,7 @@
     Use an oracle dataset to progressively determine what kind of data is necessary from self-task or transfer tasks to "succeed" in autotuning
 """
 import pandas as pd, numpy as np
-import os, time, sys, argparse, pathlib, inspect, warnings, json, logging
+import os, time, sys, argparse, pathlib, inspect, warnings, json, logging, itertools
 from importlib import import_module
 from copy import deepcopy
 from sdv.single_table import GaussianCopulaSynthesizer as GaussianCopula
@@ -20,10 +20,8 @@ except ImportError:
 def hypergeo(i,p,t,k):
     return (comb(i,t)*comb((p-i),(k-t))) / comb(p,k)
 
+# Make JSON-style logs
 class JsonLogFormatter(logging.Formatter):
-    def __init__(self):
-        super().__init__()
-        self.log_records = []
     def format(self, record):
         log_data = {
             'timestamp': self.formatTime(record),
@@ -33,10 +31,7 @@ class JsonLogFormatter(logging.Formatter):
             'funcName': record.funcName,
             'lineNo': record.lineno,
         }
-        self.log_records.append(log_data)
         return json.dumps(log_data)
-    def get_log_records(self):
-        return self.log_records
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
@@ -47,25 +42,26 @@ def build():
                             help="Number of evals (default: %(default)s)")
     experiment.add_argument("--fit-seed", type=int, default=None,
                             help="RNG seed during model fitting (default: %(default)s)")
+    experiment.add_argument("--fit-method", choices=['objective-best','objective-worst','objective-cocktail'], default='objective-best',
+                            help="Method to select data for fitting from the dataset (default: %(default)s)")
     experiment.add_argument("--sample-seed", type=int, default=None, nargs="*",
                             help="RNG seed(s) during model inference (default: %(default)s)")
-    experiment.add_argument("--filter", type=float, default=0.3,
-                            help="Filtering quantile (default: %(default)s)")
-    experiment.add_argument("--inverted-filter", action="store_true",
-                            help="Invert filtering criterion (default: %(default)s)")
+    experiment.add_argument("--inverted-objective", action="store_true",
+                            help="Invert objective criterion (default: %(default)s)")
     experiment.add_argument("--target", type=float, default=0.1,
                             help="Target quantile to meet for a seed's sampling to be considered 'successful' (default %(default)s)")
     experiment.add_argument("--hits", type=int, default=1,
-                            help="Number of samples that must be at/below target quantile per seed for the overall sampling to be considered 'successful' (default %(default)s)")
-    experiment.add_argument("--skip-auto-budget", action="store_true",
-                            help="Always use --max-evals as budet, do not attempt to derive an experiment experiment (default: %(default)s)")
-    experiment.add_argument("--always-max-evals", action="store_true",
-                            help="Calculate auto experiment, but always utilize max-evals (default: %(default)s)")
-    experiment.add_argument("--budget-ideal", type=float, default=0.1,
+                            help="Number of samples that must on-target per seed for the overall sampling to be considered 'successful' (default %(default)s)")
+    autobudget = prs.add_argument_group("Autobudgeting Settings")
+    autobudget.add_argument("--skip-auto-budget", action="store_true",
+                            help="Always use --max-evals as budet, do not attempt to derive an budget experiment (default: %(default)s)")
+    autobudget.add_argument("--always-max-evals", action="store_true",
+                            help="Calculate auto budget, but always utilize max-evals (default: %(default)s)")
+    autobudget.add_argument("--budget-ideal", type=float, default=0.1,
                             help="Ideal Proportion of the population to sample (default %(default)s)")
-    experiment.add_argument("--budget-attrition", type=float, default=0.05,
+    autobudget.add_argument("--budget-attrition", type=float, default=0.05,
                             help="Expected attrition ratio of ideal items removed by GC distribution's specificity (default %(default)s)")
-    experiment.add_argument("--budget-confidence", type=float, default=0.95,
+    autobudget.add_argument("--budget-confidence", type=float, default=0.95,
                             help="Desired confidence value for the experimented number of trials (default %(default)s)")
     files = prs.add_argument_group("File Management")
     files.add_argument("--input-data", nargs="+", required=True,
@@ -89,6 +85,10 @@ def build():
                             help="Increase n-data value per iteration (default %(default)s)")
     looping.add_argument("--to-max-n-data", action="store_true",
                             help="Always loop to --max-n-data value (default %(default)s, --max-n-data MUST ALSO be defined to take effect)")
+    looping.add_argument("--custom-data-schedule", default=None,
+                            help="Load a Numpy array (.npy) as your custom looping values (default %(default)s)")
+    looping.add_argument("--early-exit-on-failure", action="store_true",
+                            help="Exit early if any seed fails to meet its budget during few-shot experiments (default %(default)s)")
     return prs
 
 def parse(args=None, prs=None):
@@ -104,15 +104,21 @@ def parse(args=None, prs=None):
         torch.manual_seed(args.fit_seed)
     if args.sample_seed is None:
         args.sample_seed = [None]
+    # Custom data schedule
+    if args.custom_data_schedule is not None:
+        args.custom_data_schedule = np.load(open(args.custom_data_schedule,'rb'))
+        if not np.issubdtype(args.custom_data_schedule.dtype,int):
+            raise ValueError(f"Custom data schedule must use an integer type (operate as indices), got {args.custom_data_schedule.dtype}")
     # Instantiate log file handler
     if args.log_experiment is None:
         handler = logging.StreamHandler(sys.stdout)
     else:
-        handler = logging.FileHandler(args.log_experiment, mode='a')
+        handler = logging.FileHandler(args.log_experiment, mode='w')
     handler.setFormatter(JsonLogFormatter())
     logger.addHandler(handler)
     return args
 
+# Forcible module-load and attribute selection
 def import_hook_from_file(fname, attr):
     if type(fname) is not pathlib.Path:
         fname = pathlib.Path(fname)
@@ -133,6 +139,9 @@ def load_input(args):
                 load['scale'] = scale_data
             else:
                 load.insert(len(load.columns), 'scale', scale_data)
+        # For this dataset with GC, most columns need to operate as str type to ensure categories remain enforced.
+        # Pandas will detect numerical data and tell SDV to make it numerical, but this can allow GC to infer values
+        # that aren't in the full space.
         non_scale_cols = [_ for _ in load.columns if _ not in ['scale','objective']]
         load[non_scale_cols] = load[non_scale_cols].astype(str)
         concat.append(load)
@@ -168,6 +177,26 @@ def build_model(df, match_cols, args):
     model = GaussianCopula(metadata, enforce_min_max_values=False)
     model.add_constraints(constraints=constraints)
     return model, evaluator
+
+def select_fit_data(oracle, n_data, param_columns, args):
+    if (args.fit_method == 'objective-worst' and args.inverted_objective) or \
+        (args.fit_method == 'objective-best' and not args.inverted_objective):
+        return oracle.loc[range(n_data), param_columns]
+    elif (args.fit_method == 'objective-best' and args.inverted_objective) or \
+        (args.fit_method == 'objective-worst' and not args.inverted_objective):
+        mlen = len(oracle)
+        return oracle.loc[range(mlen-1,mlen-1-n_data,-1), param_columns]
+    elif args.fit_method == 'objective-cocktail':
+        # While we could cocktail-shaker-sort iteratively, we know it will be two ranges
+        # with at most one extra entry on the side we started at if the value of n_data is odd.
+        # Starting side is based on args.inverted_objective
+        mlen = len(oracle)
+        half = n_data // 2
+        extra_low = 0 if (n_data % 2 == 0 or not args.inverted_objective) else 1
+        extra_high = 0 if (n_data % 2 == 0 or args.inverted_objective) else 1
+        low_range = range(0, half+extra_low)
+        high_range = range(mlen-half-extra_high, mlen)
+        return oracle.loc[itertools.chain(low_range, high_range), param_columns]
 
 def autobudget(model, evaluator, args):
     if args.skip_auto_budget:
@@ -249,30 +278,38 @@ def evaluation(oracle, sampled, args):
     success_count = np.zeros(len(sampled), dtype=int)
     target_idx_to_beat = int(round(len(oracle) * args.target, 0))
     for (idx, samples) in enumerate(sampled):
-        if args.inverted_filter:
+        if args.inverted_objective:
             on_target = np.where(samples['objective'] > target_idx_to_beat)[0]
         else:
             on_target = np.where(samples['objective'] < target_idx_to_beat)[0]
         success_count[idx] = len(on_target)
         success[idx] = success_count[idx] >= args.hits
-    logger.debug(f"Few-shot Evaluation success count: {success_count}")
+    logger.debug(f"Few-shot Evaluation success count: {success_count} / {[_ for _ in map(len, sampled)]}")
     return success
 
 def main(args=None):
     args = parse(args)
     logger.info(args.__dict__)
+    logger.info(os.getcwd())
     oracle = load_input(args)
     param_columns = [_ for _ in oracle.columns if _ not in ['objective','predicted','elapsed_sec']]
+    logger.debug(f"Detected these columns as learnable: {param_columns}")
     model, evaluator = build_model(oracle, param_columns, args)
+    # Determine looping bounds
+    data_schedule = range(2,len(oracle),1)
+    if args.start_n_data is not None:
+        data_schedule = range(args.start_n_data, len(oracle) if args.max_n_data is None else args.max_n_data, args.step_n_data)
+    if args.custom_data_schedule is not None:
+        data_schedule = args.custom_data_schedule
     # Iteratively re-fit and re-sample on seeds
-    n_data = args.start_n_data
-    while n_data < len(oracle) and (args.max_n_data is None or n_data < args.max_n_data):
-        logger.debug(f"Begin trial with {n_data} data points")
+    for n_data in data_schedule:
+        logger.debug(f"Begin trial with {n_data} data points selected by {args.fit_method}+{'inverted-objective' if args.inverted_objective else 'objective'}")
         # Fitting process
+        fit_data = select_fit_data(oracle, n_data, param_columns, args)
         fit_model = deepcopy(model)
         with warnings.catch_warnings():
             warnings.simplefilter('ignore')
-            fit_model.fit(oracle.loc[range(n_data), param_columns])
+            fit_model.fit(fit_data)
         # Budgeting process
         budget_evals = autobudget(fit_model, evaluator, args)
         # Sampling process
@@ -282,9 +319,8 @@ def main(args=None):
         success = evaluation(oracle, sampled, args)
         if not all(success):
             logger.debug(f"{sum(~success)}/{len(success)} seed failures with {n_data} records")
-            if args.max_n_data is None or not args.to_max_n_data:
+            if args.early_exit_on_failure:
                 break
-        n_data += args.step_n_data
     logger.debug(f"Exiting on {n_data} trial")
     return args
 
@@ -292,6 +328,9 @@ if __name__ == '__main__':
     args = main()
     # On successful termination, overwrite logs with fully-compliant JSON
     if args.log_experiment is not None:
-        log_records = logger.handlers[0].formatter.get_log_records()
+        log_records = []
+        with open(args.log_experiment,'r') as accumulated_logs:
+            for line in accumulated_logs.readlines():
+                log_records.append(json.loads(line))
         json.dump(log_records, open(args.log_experiment,'w'))
 
