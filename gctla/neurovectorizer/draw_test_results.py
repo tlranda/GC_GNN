a432ba4920fcad1f9f99b016ca84f6a188ef2070
Thomas L Randall <tlranda@clemson.edu>
2024-05-09 14:30:00 -0400

    Add scripts for analyzing these things

---
+++ gctla/neurovectorizer/draw_test_results.py
@@ -0,0 +1,152 @@
+import pathlib
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1.axes_divider import make_axes_area_auto_adjustable
+
+
+def load_data(sources=None, y_labels=None, y_labels_at=None, source_range=None):
+    if y_labels_at is None:
+        y_labels_at = []
+    if y_labels is None:
+        y_labels = []
+    accumulate_colors = []
+    accumulate_passes = []
+    accumulate_n_passing = []
+    if source_range is None:
+        clipped_sources = sources
+    else:
+        clipped_sources = sources[source_range]
+    for source in clipped_sources:
+        data = pd.read_csv(pathlib.Path('test_results').joinpath(f"{source}_to_all_tests.csv")).drop(columns=['source'])
+        #data.set_index('target',inplace=True)
+        data.columns = pd.MultiIndex.from_product([[source], data.columns])
+        # Drop 'source' and 'target' columns
+        color_data = data.drop(columns=['target'], level=1)
+        if len(y_labels) == 0:
+            current_y_label = ""
+            variant_normalizer = lambda name: name.rsplit("_",1)[0]
+            skip_threshold = 100
+            skip_count = skip_threshold
+            for idx,target in enumerate(data[source,'target']):
+                normalized = variant_normalizer(target)
+                if normalized != current_y_label:
+                    if skip_count >= skip_threshold:
+                        y_labels_at.append(idx)
+                        y_labels.append(normalized)
+                        skip_count = 0
+                    else:
+                        skip_count += 1
+                    current_y_label = normalized
+            #print(len(y_labels_at))
+            #print('\n'.join(y_labels))
+        # Map boolean values to colors
+        data_colors = color_data.astype(int)
+        passes = data_colors.sum()
+        accumulate_passes.extend(passes.tolist())
+        n_passing = passes.sum()
+        accumulate_n_passing.append(n_passing)
+        accumulate_colors.append(data_colors)
+    data_colors = pd.concat(accumulate_colors, axis=1)
+    return y_labels, y_labels_at, data_colors, accumulate_passes, accumulate_n_passing
+
+def plot_image(data_colors, accumulate_passes, accumulate_n_passing, y_labels_at, y_labels, source_range=None):
+    # Plot the image
+    fig = plt.figure(figsize=(18, 6))
+    # Make x-axis labels
+    n_groupings = len(data_colors.columns.levels[0])
+    n_subcols = len(data_colors.columns.levels[1])
+    xlabels = [f"{col[2:]}"+('\n' if index_count % 2 == 0 else '\n|\n')+f"{npass}" for index_count, (col,npass) in enumerate(zip(data_colors.columns.levels[1].tolist()*len(data_colors.columns.levels[0]), accumulate_passes))]
+    bonus_xlabels = [] #['' for _ in xlabels]
+    for meta_idx, idx in enumerate([(_*n_subcols)+(n_subcols//2) for _ in range(n_groupings)]):
+        #xlabels[idx] += "\nBONUS"
+        bonus_xlabels.append(f"{data_colors.columns.levels[0][meta_idx]}"+("\n" if meta_idx % 2 == 0 else "\n|\n")+f"{accumulate_n_passing[meta_idx]}")
+    #print(xlabels)
+    #print(bonus_xlabels)
+
+    ax = sns.heatmap(data_colors, cmap=['white', 'black'], cbar=False,
+                     yticklabels=False,
+                     xticklabels=False)
+    # Add names for sources
+    ax2 = ax.twiny()
+    xticks = [(_/n_groupings)+(1/(2*n_groupings)) for _ in list(range(n_groupings))]
+    #print(xticks)
+    ax2.set(xticks=xticks, xticklabels=bonus_xlabels)
+
+    # Add separators between sources
+    for idx, grouping in enumerate(data_colors.columns.levels[0]):
+        ax.vlines((idx+1)*n_subcols,0,len(data_colors), color='k')
+    # Add names for some targets
+    ax.set(yticks=y_labels_at, yticklabels=y_labels)
+    ax.set(xticks=0.5+np.arange(n_groupings*n_subcols), xticklabels=xlabels)
+    #make_axes_area_auto_adjustable(ax2)
+    #make_axes_area_auto_adjustable(ax)
+    n_tests = len(data_colors) * len(data_colors.columns)
+    fig.suptitle(f"Transferability out of {n_tests} Targets")
+    fig.set_tight_layout(True)
+    #plt.show()
+    figname = f"Plot_for_slice_{source_range.start:04d}_{source_range.stop:04d}.png"
+    fig.savefig(figname, format='png')
+    print("\tPlotted:", figname)
+    plt.close(fig)
+
+def main():
+    sources = np.asarray([_.stem[:-len('_to_all_tests')] for _ in pathlib.Path('test_results').iterdir() if _.suffix == '.csv'])
+    y_labels, y_labels_at = None, None
+    per_slice = 15
+    n_slices = (sources.shape[0]+(per_slice-1)) // per_slice
+    for (start,stop) in zip(np.arange(0,n_slices*per_slice,per_slice), np.arange(per_slice,n_slices*per_slice,per_slice)):
+        stop = min(stop,sources.shape[0])
+        if start == stop:
+            break
+        print(f"Process slice {start} -> {stop}")
+        y_labels, y_labels_at, data_colors, accumulate_passes, accumulate_n_passing = load_data(sources=sources,
+                                                                                                y_labels=y_labels,
+                                                                                                y_labels_at=y_labels_at,
+                                                                                                source_range=slice(start,stop))
+        plot_image(data_colors, accumulate_passes, accumulate_n_passing, y_labels_at, y_labels, source_range=slice(start,stop))
+
+if __name__ == '__main__':
+    main()
+"""
+sources = np.asarray([_.stem[:-len('_to_all_tests')] for _ in pathlib.Path('test_results').iterdir() if _.suffix == '.csv'])
+source_range = slice(0,15)
+sources = sources[source_range]
+y_labels_at = []
+y_labels = []
+current_y_label = ""
+variant_normalizer = lambda name: name.rsplit("_",1)[0]
+skip_threshold = 100
+skip_count = skip_threshold
+accumulate_colors = []
+accumulate_passes = []
+accumulate_n_passing = []
+for source in sources:
+    data = pd.read_csv(pathlib.Path('test_results').joinpath(f"{source}_to_all_tests.csv")).drop(columns=['source'])
+    #data.set_index('target',inplace=True)
+    data.columns = pd.MultiIndex.from_product([[source], data.columns])
+    # Drop 'source' and 'target' columns
+    color_data = data.drop(columns=['target'], level=1)
+    if len(y_labels) == 0:
+        for idx,target in enumerate(data[source,'target']):
+            if variant_normalizer(target) != current_y_label:
+                if skip_count >= skip_threshold:
+                    y_labels_at.append(idx)
+                    y_labels.append(variant_normalizer(target))
+                    skip_count = 0
+                else:
+                    skip_count += 1
+                current_y_label = variant_normalizer(target)
+        #print(len(y_labels_at))
+        #print('\n'.join(y_labels))
+    # Map boolean values to colors
+    data_colors = color_data.astype(int)
+    passes = data_colors.sum()
+    accumulate_passes.extend(passes.tolist())
+    n_passing = passes.sum()
+    accumulate_n_passing.append(n_passing)
+    n_tests = len(data_colors) * len(data_colors.columns)
+    accumulate_colors.append(data_colors)
+data_colors = pd.concat(accumulate_colors, axis=1)
+"""
---
+++ gctla/neurovectorizer/mass_parallel_tester.py
@@ -0,0 +1,50 @@
+import subprocess
+import pathlib
+import multiprocessing
+import tqdm
+import pandas as pd
+import numpy as np
+
+def run_comparison(source, target, basic_script_call, index):
+    script_call = basic_script_call + f" --source {source} --target {target}"
+    proc = subprocess.run(script_call.split(' '), capture_output=True)
+    if proc.returncode != 0:
+        raise ValueError
+    lines = proc.stdout.decode('utf-8').split('\n')
+    results = {'source': source.stem, 'target': target.stem}
+    for line in lines:
+        if "Test PASS" in line:
+            testid = line.split(' ')[0]
+            results[testid] = True
+        elif "Test FAIL" in line:
+            testid = line.split(' ')[0]
+            results[testid] = False
+    return index, results
+
+if __name__ == "__main__":
+    basic_script_call = "python3 tl_checks.py --perf-column runtime --exhaustive --hyp1"
+    csvs = np.asarray([_ for _ in pathlib.Path('neurovectorizer-titan/per-app/').iterdir() if _.suffix == '.csv'])
+    n_csvs = len(csvs)
+    n_expect = n_csvs - 1
+    for csv_idx, source in enumerate(csvs):
+        targets = np.hstack((csvs[:csv_idx],csvs[csv_idx+1:]))
+        outputfile = pathlib.Path('test_results').joinpath(f'{source.stem}_to_all_tests.csv')
+        if outputfile.exists() and len(pd.read_csv(outputfile)) == n_expect:
+            print(f"Skip {outputfile} -- All tests done :)")
+            continue
+        print(f"Run tests for {source}")
+        with multiprocessing.Pool() as pool:
+            result_queue = []
+            print("Making asynchronous requests")
+            for idx, target in tqdm.tqdm(enumerate(targets), total=n_expect):
+                args = (source, target, basic_script_call, idx)
+                result_queue.append(pool.apply_async(run_comparison, args))
+            print("Retrieving asynchronous requests")
+            results = [None for _ in range(n_expect)]
+            for _ in tqdm.tqdm(result_queue, total=n_expect):
+                idx, async_results = _.get()
+                results[idx] = async_results
+        accumulate = pd.DataFrame(results)
+        print(accumulate)
+        accumulate.to_csv(outputfile, index=False)
+
