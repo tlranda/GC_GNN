58fec1921005661b5dafe91a79bd97d525356bb8
Thomas L Randall <tlranda@clemson.edu>
2024-10-10 12:43:25 -0500

    Fixes OpenTuner issues for base class / 3mm, to be forwarded to other implementations

---
+++ opentuner/_3mm/_3mm_opentuner.py
@@ -0,0 +1,81 @@
+import opentuner
+from opentuner import ConfigurationManipulator
+from opentuner import EnumParameter
+from opentuner import MeasurementInterface
+from opentuner import Result
+
+import pandas as pd
+import numpy as np
+
+import random
+import pathlib
+import warnings
+
+from opentuner_class import OpenTuner_Tuner
+from ytopt.benchmark.base_plopper import Polybench_Plopper
+#from GC_TLA.base_plopper import Polybench_Plopper
+
+"""
+    Based on demo: https://opentuner.org/tutorial/gettingstarted,
+    adapted to lookup results from existing dataset
+"""
+
+class _3mm_Tuner(OpenTuner_Tuner):
+    def pmetric(self, timing_list):
+        return np.asarray(timing_list)[:,1:].mean()
+
+    def build(self):
+        prs, opentuner_args, extra_args = super().build()
+        _3mm = prs.add_argument_group('3MM')
+        _3mm.add_argument('--dataset-size', choices=['SM','XL'], default=None, required=True,
+                          help="Problem size to tune")
+        return prs, opentuner_args, extra_args
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.dataset_size = self.args.dataset_size
+
+        template = pathlib.Path('./mmp.c').resolve()
+        self.plopper = Polybench_Plopper(str(template), str(template.parents[0]), output_extension='.c')
+        self.plopper.metric = self.pmetric
+
+        self.configure_desired_result([f'p{_}' for _ in range(10)]+['objective'])
+        self.opentuner_results = pd.DataFrame(columns=sorted(self.opentuner_params))
+
+    def manipulator(self):
+        """
+        Define the search space by creating a ConfigurationManipulator
+        """
+        manipulator = ConfigurationManipulator()
+        # Options are not weighted by order, first option in list is default
+        parameters = [
+                EnumParameter('p0', [" ", "#pragma clang loop(j2) pack array(A) allocate(malloc)"]),
+                EnumParameter('p1', [" ", "#pragma clang loop(i1) pack array(B) allocate(malloc)"]),
+                EnumParameter('p2', [" ", "#pragma clang loop(i1,j1,k1,i2,j2) interchange permutation(j1,k1,i1,j2,i2)"]),
+                EnumParameter('p3', ['256','4','8','16','20','32','50','64','80','96','100','128','512','1024','2048']),
+                EnumParameter('p4', ['256','4','8','16','20','32','50','64','80','100','128','512','1024','2048']),
+                EnumParameter('p5', ['256','4','8','16','20','32','50','64','80','100','128','512','1024','2048']),
+                EnumParameter('p6', [" ", "#pragma clang loop(j2) pack array(C) allocate(malloc)"]),
+                EnumParameter('p7', [" ", "#pragma clang loop(i1) pack array(D) allocate(malloc)"]),
+                EnumParameter('p8', [" ", "#pragma clang loop(j2) pack array(E) allocate(malloc)"]),
+                EnumParameter('p9', [" ", "#pragma clang loop(i1) pack array(F) allocate(malloc)"]),
+                ]
+        for parameter in parameters:
+            manipulator.add_parameter(parameter)
+        return manipulator
+
+    def run(self, desired_result, input, limit):
+        """
+        'Compile' and 'run' a given configuration, then return performance
+        """
+        cfg = desired_result.configuration.data
+        cfg_params = dict((k.upper(),cfg[k]) for k in [f'p{_}' for _ in range(10)])
+        result_time = self.plopper.findRuntime(list(cfg_params.values()), list(cfg_params.keys()), f" -D{self.dataset_size}_DATASET")
+        cfg['objective'] = result_time
+        self.add_opentuner_result(desired_result)
+        return Result(time=result_time)
+
+if __name__ == '__main__':
+    _3mm_Tuner().main()
+
--- opentuner/opentuner_class.py
+++ opentuner/opentuner_class.py
@@ -2,7 +2,9 @@
 
 # Builtin imports
 import argparse
+from copy import deepcopy
 import random
+import sys
 from typing import Optional, List, Tuple, Union
 import warnings
 
@@ -20,17 +22,18 @@ class OpenTuner_Tuner(MeasurementInterface):
     # Default argument values declared at class namespace so subclasses can
     # easily redefine them when defaults prove to be inadequate.
     # Refer to --help or the build() method for semantic meanings
-    default_max_calls = 30
+    default_max_calls = 200
     default_seed = 1234
 
     def __init__(self, *args, **kwargs):
         # Set basic arguments for the script
-        self.prs = self.build()
-        self.args = self.parse()
+        self.prs, self.opentuner_args, extra_args = self.build()
+        temp_args = self.parse(extra_args, namespace=deepcopy(self.opentuner_args))
 
         # Repackage and push on to normal initialization
-        args = tuple([self.args] + list(args))
-        super().__init__(*args, **kwargs)
+        super().__init__(self.opentuner_args, **kwargs)
+        # Reset args to full list
+        self.args = temp_args
 
     """
         IMPLEMENTERS WILL OVERRIDE THESE METHODS
@@ -42,28 +45,32 @@ class OpenTuner_Tuner(MeasurementInterface):
             # Then your own implementation / extension here
     """
 
-    def build(self) -> argparse.ArgumentParser:
+    def build(self) -> Tuple[argparse.ArgumentParser, argparse.Namespace, List[str]]:
         # Simplify the argparse process of indicating a default argument in the help string
         default_help = "(default: %(default)s)"
 
         # Get basic interface from opentuner
         prs = opentuner.default_argparser()
+        if not any([_ in sys.argv for _ in ['-h','--help']]):
+            opentuner_args, remaining_args = prs.parse_known_args()
+        else:
+            # Help will be processed during parse(), after all build()s have passed
+            opentuner_args = None
+            remaining_args = None
         # General extensions
         general = prs.add_argument_group("General")
-        general.add_argument("--seed", type=int, default=self.default_seed,
+        general.add_argument("--random-seed", type=int, default=self.default_seed,
                              help=f"Random seed {default_help}")
         general.add_argument("--csv-output", default=None,
                              help="Filename to save results to (default: print to output as Pandas Dataframe)")
-        return prs
+        return prs, opentuner_args, remaining_args
 
-    def parse(self) -> argparse.Namespace:
-        self.args = self.prs.parse_args()
-
-        # Check values
+    def parse(self, args=None, namespace=None) -> argparse.Namespace:
+        self.args = self.prs.parse_args(args, namespace=namespace)
 
         # Set seeds
-        random.seed(self.args.seed)
-        np.random.seed(self.args.seed)
+        random.seed(self.args.random_seed)
+        np.random.seed(self.args.random_seed)
 
         return self.args
 
@@ -89,6 +96,9 @@ class OpenTuner_Tuner(MeasurementInterface):
         #
         # Always append key results in a pandas dataframe under the attribute
         # 'opentuner_results' to ensure proper exports at the end of tuning
+        # You may find it easier to track this via helper methods:
+        # * configure_desired_result()
+        # * add_opentuner_result()
         #
         # Return a Result object a la:
         # return Result(time=objective_time)
@@ -99,8 +109,21 @@ class OpenTuner_Tuner(MeasurementInterface):
         (unless you intend to change how OpenTuner itself operates)
     """
 
-    def main(self):
-        super().main(self.args)
+    def configure_desired_result(self, params):
+        # Call this during __init__ or manipulator() to set up the parameters that are saved from your desired_result in add_opentuner_result()
+        if hasattr(self,'opentuner_params'):
+            self.opentuner_params.add(set(params))
+        else:
+            self.opentuner_params = set(params)
+
+    def add_opentuner_result(self, desired_result, **kwargs):
+        cfg = desired_result.configuration.data
+        cfg.update(kwargs)
+        cfg = dict((k,cfg[k]) for k in sorted(self.opentuner_params))
+        new_result = pd.DataFrame(cfg, columns=sorted(self.opentuner_params),
+                                  index=[len(self.opentuner_results)])
+        self.opentuner_results = pd.concat((self.opentuner_results,
+                                            new_result))
 
     def save_final_config(self, configuration):
         # Called at the end of tuning
@@ -109,6 +132,9 @@ class OpenTuner_Tuner(MeasurementInterface):
         else:
             self.opentuner_results.to_csv(self.args.csv_output, index=False)
         print(f"Optimal result: {configuration.data}")
+
+    def main(self):
+        super().main(self.args)
 
 if __name__ == '__main__':
     OpenTuner_Tuner().main()
