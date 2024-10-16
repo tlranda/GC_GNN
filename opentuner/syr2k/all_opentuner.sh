e6bcfd321a658a5fd247421351305ad0fe2dc87e
tlranda <tlranda@clemson.edu>
2024-10-16 17:29:56 -0400

    Fixes opentuner via syr2k testing, updates scripts to skip known-to-be-broken techniques

--- opentuner/syr2k/all_opentuner.sh
+++ opentuner/syr2k/all_opentuner.sh
@@ -4,11 +4,28 @@
 for size in "SM" "XL" ; do
     for seed in "1234" "2024" "9999" ; do
         mkdir -p opentuner_results/${size}/seed_${seed};
-        for strat in `PYTHONPATH=.. python3 syr2k_opentuner.py -lt`; do
-            echo $strat;
-            PYTHONPATH=.. python3 syr2k_opentuner.py --test-limit 200 --size ${size} --no-dups --technique ${strat} --random-seed ${seed} --output opentuner_results/${size}/seed_${seed}/opentuner_${strat}.csv;
-            if [[ $? -ne 0 ]]; then
-                echo "Failed opentuner ${size} ${seed} ${strat}" >> opentuner_failures.log;
+        strategies=$( PYTHONPATH=.. python3 syr2k_opentuner.py -lt | grep -v -e "PatternSearch" -e "pso-OX3" -e "pso-OX1" -e "RegularNelderMead" -e "RightNelderMead" -e "RegularTorczon" -e "RightTorczon" -e "MultiNelderMead" -e "MultiTorczon");
+        for strat in ${strategies[@]}; do
+            expect="opentuner_results/${size}/seed_${seed}/opentuner_${strat}.csv";
+            todo="PYTHONPATH=.. python3 syr2k_opentuner.py --test-limit 200 --size ${size} --no-dups --technique ${strat} --random-seed ${seed} --csv-output ${expect}";
+            if [[ -e ${expect} ]]; then
+                line_count=$( wc -l ${expect} | awk '{print $1}');
+                if [[ ${line_count} -ge 200 ]]; then
+                    echo "OK ${expect} has expected 200 lines -- Skipping";
+                else
+                    echo "File ${expect} exists with ${line_count} lines, running to get 200";
+                    echo $todo;
+                    eval $todo;
+                    if [[ $? -ne 0 ]]; then
+                        echo "Failed opentuner ${size} ${seed} ${strat}" >> opentuner_failures.log;
+                    fi
+                fi
+            else
+                echo $todo;
+                eval $todo;
+                if [[ $? -ne 0 ]]; then
+                    echo "Failed opentuner ${size} ${seed} ${strat}" >> opentuner_failures.log;
+                fi
             fi
         done;
     done;
