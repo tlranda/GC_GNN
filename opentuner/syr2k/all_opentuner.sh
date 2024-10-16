4972232afdb655205df320991f373df6ef090728
Thomas L Randall <tlranda@clemson.edu>
2024-10-16 14:55:38 -0400

    Job script updates

--- opentuner/syr2k/all_opentuner.sh
+++ opentuner/syr2k/all_opentuner.sh
@@ -4,9 +4,9 @@
 for size in "SM" "XL" ; do
     for seed in "1234" "2024" "9999" ; do
         mkdir -p opentuner_results/${size}/seed_${seed};
-        for strat in `python3 syr2k_opentuner.py -lt`; do
+        for strat in `PYTHONPATH=.. python3 syr2k_opentuner.py -lt`; do
             echo $strat;
-            python3 syr2k_opentuner.py --test-limit 30 --syr2k-size ${size} --no-dups --technique ${strat} --output opentuner_results/${size}/seed_${seed}/opentuner_${strat}.csv --seed ${seed};
+            PYTHONPATH=.. python3 syr2k_opentuner.py --test-limit 200 --size ${size} --no-dups --technique ${strat} --random-seed ${seed} --output opentuner_results/${size}/seed_${seed}/opentuner_${strat}.csv;
             if [[ $? -ne 0 ]]; then
                 echo "Failed opentuner ${size} ${seed} ${strat}" >> opentuner_failures.log;
             fi
