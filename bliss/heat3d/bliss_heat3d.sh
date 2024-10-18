8c70abda197f0215aed66917f3c583629332917d
Thomas L Randall <tlranda@clemson.edu>
2024-10-18 14:50:17 -0500

    Swap plopper for heat3d and requeue with new script format

--- bliss/heat3d/bliss_heat3d.sh
+++ bliss/heat3d/bliss_heat3d.sh
@@ -9,8 +9,9 @@ for size in "SM" "XL" ; do
     mkdir -p bliss_results/${size};
     for seed in "1234" "2024" "9999" ; do
         date;
-        echo "PYTHONPATH=.. python3 heat3d_bliss.py --max-calls 200 --size ${size} --seed ${seed} --csv-output bliss_results/${size}/bliss_seed_${seed}.csv;";
-        PYTHONPATH=.. python3 heat3d_bliss.py --max-calls 200 --size ${size} --seed ${seed} --csv-output bliss_results/${size}/bliss_seed_${seed}.csv;
+        todo="PYTHONPATH=.. python3 heat3d_bliss.py --max-calls 200 --size ${size} --seed ${seed} --csv-output bliss_results/${size}/bliss_seed_${seed}.csv;";
+        echo "${todo}";
+        eval "${todo}";
         if [[ $? -ne 0 ]]; then
             echo "[ $(date) ] Failed bliss ${size} ${seed}" >> bliss_failures.log;
         fi
