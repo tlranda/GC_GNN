f280f25a545439aaf7533aefad80cd1ebb6a3a40
Thomas L Randall <tlranda@clemson.edu>
2024-11-27 10:48:19 -0600

    3mm OpenTuner

--- opentuner/_3mm/opentuner_3mm.sh
+++ opentuner/_3mm/opentuner_3mm.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-#source /home/trandall/set_swing_environment.sh;
-#module remove nvhpc;
+source /home/trandall/set_swing_environment.sh;
+module remove nvhpc;
 cd /home/trandall/GC_GNN/opentuner/_3mm/;
 
 #techniques=$( PYTHONPATH=.. python3 _3mm_opentuner.py -lt --size SM );
-techniques=( "PerfectRandom" "OS2" );
+techniques=( "AUCBanditMetaTechniqueB" "NormalGreedyMutation10" "ga-PX" );
 for technique in ${techniques[@]}; do
     for size in "SM" "XL"; do
         mkdir -p opentuner_results/${technique}/${size};
         for seed in "1234" "2024" "9999"; do
+            expect="opentuner_results/${technique}/${size}/opentuner_seed_${seed}.csv";
             date;
-            todo="PYTHONPATH=.. python3 _3mm_opentuner.py --size ${size} --random-seed ${seed} --technique ${technique} --csv-output opentuner_results/${technique}/${size}/opentuner_seed_${seed}.csv";
+            if [[ -f "${expect}" ]]; then
+                todo="wc -l ${expect}";
+            else
+                todo="PYTHONPATH=.. python3 _3mm_opentuner.py --size ${size} --random-seed ${seed} --test-limit 200 --technique ${technique} --csv-output ${expect}";
+            fi
             echo $todo;
-            #eval $todo;
+            eval $todo;
             if [[ $? -ne 0 ]]; then
                 echo "[ $(date) ] Failed OpenTuner ${size} ${seed} ${technique}" >> opentuner_failures.log;
             fi
