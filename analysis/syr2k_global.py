bea474dbcfc4084d5ebc91cecac7840d6f537bae
Thomas L Randall <tlranda@clemson.edu>
2025-03-28 13:05:47 -0400

    Plotting updates for figure 4ab

--- analysis/syr2k_global.py
+++ analysis/syr2k_global.py
@@ -6,17 +6,33 @@ import matplotlib.pyplot as plt
 import pathlib
 
 SM = pd.read_csv('collations/syr2k_all_SM.csv').sort_values(by=['objective']).reset_index(drop=True)
-XL = pd.read_csv('collations/syr2k_all_XL.csv').sort_values(by=['objective'])[4:].reset_index(drop=True)
+XL = pd.read_csv('collations/syr2k_all_XL.csv').sort_values(by=['objective'])[5:].reset_index(drop=True)
 
-markers = {'quick': '+',
+markers = {'quick': 'x',
            'ultimate': 'd',
            }
-colors = {'GC': 'red',
-          'GPTune': 'purple',
-          'BLISS': 'green',
-          'OpenTuner': 'orange',
+colors = {'GC+NCS': 'blue',
+          'GC': 'orange',
+          'GPTune': 'green',
+          'BLISS': 'yellow',
+          'OpenTuner': 'black',
           }
-CUTOFF = 100
+voffset = {'GC+NCS': 0,
+           'GC': 1,
+           'GPTune': 2,
+           'BLISS': 3,
+           'OpenTuner': 4,
+           }
+zorder = {'GC+NCS': 4,
+          'GC': 3,
+          'GPTune': 2,
+          'BLISS': 1,
+          'OpenTuner': 0,
+          }
+CUTOFF = 30
+ALPHA = 0.5
+SIZE = 200
+LIVE_PLOTTING = False
 
 cols = [f'p{_}' for _ in range(6)]
 
@@ -36,11 +52,12 @@ def lookup(search, oracle, csvname):
         raise ValueError
     return find[0]
 
-def add_to_plot(csv, labeled_SM, labeled_XL, figSM, axSM, figXL, axXL):
+def add_to_plot(csv, labeled_SM, labeled_XL, figSM, axSM, figXL, axXL, legendSM, legendXL):
     if ("SM" not in csv.name and "XL" not in csv.name) or "default" in csv.name:
-        return labeled_SM, labeled_XL, figSM, axSM, figXL, axXL
+        return labeled_SM, labeled_XL, figSM, axSM, figXL, axXL, legendSM, legendXL
     # Pick our axis
     if "SM" in csv.name:
+        picked_name = "SM"
         picked_data = SM
         # Initialize our axis as needed
         if figSM is None:
@@ -49,7 +66,9 @@ def add_to_plot(csv, labeled_SM, labeled_XL, figSM, axSM, figXL, axXL):
         picked_fig = figSM
         picked_ax = axSM
         picked_labels = labeled_SM
+        picked_legend = legendSM
     else:
+        picked_name = "XL"
         picked_data = XL
         # Initialize our axis as needed
         if figXL is None:
@@ -58,6 +77,12 @@ def add_to_plot(csv, labeled_SM, labeled_XL, figSM, axSM, figXL, axXL):
         picked_fig = figXL
         picked_ax = axXL
         picked_labels = labeled_XL
+        picked_legend = legendXL
+    quant_heights = np.logspace(np.log2(picked_data['objective'].min()),
+                                np.log2(picked_data['objective'].quantile(0.5)),
+                                len(voffset),
+                                base=2.0)
+    #print(f"{picked_name} has min {picked_data['objective'].min()} and qh {quant_heights}")
 
     search = pd.read_csv(csv)
     if 'actually_measured' in search.columns:
@@ -66,7 +91,9 @@ def add_to_plot(csv, labeled_SM, labeled_XL, figSM, axSM, figXL, axXL):
     if min(search['objective']) < 0:
         search['objective'] *= -1
 
-    if 'GaussianCopula' in str(csv):
+    if 'reordered_search' in str(csv):
+        kind = 'GC+NCS'
+    elif 'GaussianCopula' in str(csv):
         kind = 'GC'
     elif 'GPTune' in str(csv):
         kind = 'GPTune'
@@ -74,47 +101,90 @@ def add_to_plot(csv, labeled_SM, labeled_XL, figSM, axSM, figXL, axXL):
         kind = 'BLISS'
     elif 'opentuner' in str(csv):
         kind = 'OpenTuner'
-    quick = lookup(search.loc[:CUTOFF], picked_data, csv)
-    ult = lookup(search, picked_data, csv)
-    label = f"First {CUTOFF} {kind}" if (kind,'quick') not in picked_labels else None
+    worst = search.loc[[search['objective'].argmax()]].reset_index(drop=True)
+    quick = lookup(worst, picked_data, csv)
+    # LOG PLOTS CAN'T USE X=0
+    quick_x = 0.1 if quick == 0 else quick
+    label = f"Worst {kind}" if (kind,'quick') not in picked_labels else None
     picked_labels.add((kind,'quick'))
-    picked_ax.scatter(quick, picked_data.loc[quick,'objective'],
-                      s=200, alpha=0.5, label=label,
+    quick_y = picked_data.loc[quick,'objective']
+    quickd = picked_ax.scatter(quick_x, quant_heights[voffset[kind]],
+                      s=SIZE, alpha=ALPHA, label=label,
                       c=colors[kind], marker=markers['quick'],
+                      zorder=zorder[kind],
                       )
+    if kind not in picked_legend:
+        picked_legend[kind] = {'quick': None, 'ult': None}
+    if picked_legend[kind]['quick'] is None:
+        picked_legend[kind]['quick'] = quickd
+        print(f"Add {quickd.get_label()} to legend under {kind}/quick")
+    picked_ax.vlines(quick_x, quick_y, quant_heights[voffset[kind]],
+                     colors=colors[kind], zorder=voffset[kind])
+
+    ult = lookup(search, picked_data, csv)
+    # LOG PLOTS CAN'T USE X=0
+    ult_x = 0.1 if ult == 0 else ult
     label = f"Best {kind}" if (kind,'ultimate') not in picked_labels else None
     picked_labels.add((kind,'ultimate'))
-    picked_ax.scatter(ult, picked_data.loc[ult,'objective'],
-                      s=200, alpha=0.5, label=label,
+    ult_y = picked_data.loc[ult,'objective']
+    ultd = picked_ax.scatter(ult_x, quant_heights[voffset[kind]],
+                      s=SIZE, alpha=ALPHA, label=label,
                       c=colors[kind], marker=markers['ultimate'],
+                      zorder=zorder[kind],
                       )
-
-    return labeled_SM, labeled_XL, figSM, axSM, figXL, axXL
+    if kind not in picked_legend:
+        picked_legend[kind] = {'quick': None, 'ult': None}
+    if picked_legend[kind]['ult'] is None:
+        picked_legend[kind]['ult'] = ultd
+        print(f"Add {ultd.get_label()} to legend under {kind}/ult")
+    picked_ax.vlines(ult_x, ult_y, quant_heights[voffset[kind]],
+                     colors=colors[kind], zorder=zorder[kind])
+    #print(f"Plotted {csv} as {kind} on {picked_name} @ ({quick} & {ult})")
+    #print(f"Plotted {csv} as {kind} on {picked_name} @ {ult_x}")
+    return labeled_SM, labeled_XL, figSM, axSM, figXL, axXL, legendSM, legendXL
 
 fig_SM, ax_SM = None, None
 fig_XL, ax_XL = None, None
 labeled_SM = set()
 labeled_XL = set()
+legendSM, legendXL = {}, {}
 
 for original_search in sorted(pathlib.Path('rawsearches/syr2k').iterdir()):
-    labeled_SM, labeled_XL, fig_SM, ax_SM, fig_XL, ax_XL = add_to_plot(original_search, labeled_SM, labeled_XL, fig_SM, ax_SM, fig_XL, ax_XL)
+    (labeled_SM, labeled_XL,
+    fig_SM, ax_SM, fig_XL, ax_XL,
+    legendSM, legendXL) = add_to_plot(original_search, labeled_SM, labeled_XL,
+                                     fig_SM, ax_SM, fig_XL, ax_XL,
+                                     legendSM, legendXL)
 
-#for original_search in pathlib.Path('reordered_searches/syr2k').iterdir():
-#    fig_SM, ax_SM, fig_XL, ax_XL = add_to_plot(original_search, fig_SM, ax_SM, fig_XL, ax_XL)
+for original_search in pathlib.Path('reordered_searches/syr2k').iterdir():
+    if 'GaussianCopula' not in original_search.stem:
+        continue
+    (labled_SM, labeled_XL,
+    fig_SM, ax_SM, fig_XL, ax_XL,
+    legendSM, legendXL) = add_to_plot(original_search, labeled_SM, labeled_XL,
+                                     fig_SM, ax_SM, fig_XL, ax_XL,
+                                     legendSM, legendXL)
 
-ax_SM.legend()
-#ax_SM.set_title("Syr2k SM Exhaustive Results")
-ax_SM.set_xscale('log')
-ax_SM.set_xlabel('Optimality Rank (lower is better)')
-ax_SM.set_yscale('log')
-ax_SM.set_ylabel('Runtime (lower is better)')
-ax_XL.legend()
-#ax_XL.set_title("Syr2k XL Exhaustive Results")
-ax_XL.set_xscale('log')
-ax_XL.set_xlabel('Optimality Rank (lower is better)')
-ax_XL.set_yscale('log')
-ax_XL.set_ylabel('Runtime (lower is better)')
-fig_SM.savefig('syr2k_global_SM.png',dpi=300)
-fig_XL.savefig('syr2k_global_XL.png',dpi=300)
-#plt.show()
+for fig, ax, legend_materials, size in zip([fig_SM, fig_XL],
+                                           [ax_SM, ax_XL],
+                                           [legendSM, legendXL],
+                                           ['SM', 'XL']):
+    legend_order = list(reversed(list(voffset.keys())))
+    legend_data = [legend_materials[_]['ult'] for _ in legend_order]+\
+                  [legend_materials[_]['quick'] for _ in legend_order]
+    ax.legend(legend_data, [_.get_label() for _ in legend_data], ncol=2, loc="upper right")
+    if LIVE_PLOTTING:
+        ax.set_title(f"Syr2k {size} Exhaustive Results")
+    ax.set_xscale('log')
+    ax.set_xlabel('Optimality Rank (lower is better)')
+    ax.set_yscale('log')
+    ax.set_ylabel('Runtime (lower is better)')
+    used_xticks = ax.get_xticklabels()[2:-2]
+    positions = [_.get_position()[0] for _ in used_xticks]
+    texts = [_.get_text() for _ in used_xticks]
+    ax.set_xticks(positions, ['0']+texts[1:])
+    if not LIVE_PLOTTING:
+        fig.savefig(f'syr2k_global_{size}.png',dpi=300)
+if LIVE_PLOTTING:
+    plt.show()
 
